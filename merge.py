3d0bedc1a0cd6f08a8d062ce85bb7129269d5da6
Sebastian <sebichon124@gmail.com>
2026-06-16 13:17:19 -0500

    Más líneas de código

--- merge.py
+++ merge.py
@@ -1,18 +1,28 @@
 import pandas as pd
 import os
 
-cerebritos = pd.read_csv("resultados_iqs_1206.csv")
+cerebritos = pd.read_csv("Reporte_cerebros_IQS.csv")
 info_usuario = pd.read_csv("bq-results-20260612-141807-1781273916354 - bq-results-20260612-141807-1781273916354.csv")
+print(info_usuario)
 
-columnas_necesarias = ['Centro_Faro', 'Provincia', 'Distrito', 'incremental', 'PROFILE_CELL_PHONE']
+columnas_editables = ['Centro_Faro', 'Provincia', 'Distrito', 'incremental', 'PROFILE_CELL_PHONE']
+columnas_totales = ['Centro_Faro', 'Provincia', 'Distrito', 'incremental', 'PROFILE_CELL_PHONE', 'full_name']
 
-info_usuario = info_usuario[columnas_necesarias]
+
+info_usuario = info_usuario[columnas_totales]
 info_usuario['incremental'] = info_usuario['incremental'].astype('Int64')
 columnas_split = ['Provincia', 'Distrito']
 
 info_usuario[columnas_split] = info_usuario[columnas_split].apply(lambda col: col.str.split("-").str[1].str.strip().fillna("SIN INFORMACIÓN"))
 info_usuario['Centro_Faro'] = info_usuario['Centro_Faro'].fillna("SIN INFORMACIÓN") 
+info_usuario[columnas_editables] = info_usuario[columnas_editables].replace(
+    r'^\s*$', 
+    "SIN INFORMACIÓN", 
+    regex=True
+)
+info_usuario['PROFILE_CELL_PHONE'] = info_usuario['PROFILE_CELL_PHONE'].replace(" ", "").replace("-", "").replace("(", "").replace(")", "").str.strip()
 info_usuario = info_usuario.drop_duplicates(subset=['incremental'])
+print(info_usuario)
 
 df = pd.merge(
     left=cerebritos,
@@ -24,8 +34,15 @@ df = pd.merge(
 
 df = df.drop(columns=['incremental'])
 df = df.rename(columns={'value': 'Número de Cerebritos', 
-                        'Centro_faro': 'Centro Educativo',
+                        'Centro_Faro': 'Centro Educativo',
                         'Provincia': 'Regional',
-                        'PROFILE_CELL_PHONE': 'Teléfono'})
+                        'PROFILE_CELL_PHONE': 'Teléfono',
+                        'full_name': 'Nombre Completo'})
 
-df.to_csv("Cerebritos_IQS.csv", index=False)
+columnas_limpieza = ['Nombre Completo', 'Número de Cerebritos', 'Centro Educativo', 'Regional', 'Teléfono', 'Distrito']
+
+df[columnas_limpieza] = df[columnas_limpieza].fillna("SIN INFORMACIÓN")
+df = df.drop(columns='Nombre')
+print(df)
+
+df.to_csv("Cerebritos_IQS_16_06_2026.csv", index=False)
